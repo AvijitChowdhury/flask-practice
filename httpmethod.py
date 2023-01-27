5cf5250a2dd0211a5a1b1af0dc375d3f413ee548
Avijit Chowdhury <abhichy30@gmail.com>
2023-01-27 21:56:55 +0600

    http method

---
+++ httpmethod.py
@@ -0,0 +1,22 @@
+from flask import Flask, redirect ,url_for,request
+
+app =Flask(__name__)
+@app.route('/')
+def home():
+    return "Welcome to avijit's page"
+
+@app.route('/welcome/<name>')
+def welcome(name):
+    return 'welcome %s'%name
+
+@app.route('/login/',methods=['POST','GET'])
+def login():
+    if request.method =='POST':
+        user=request.form['name']
+        return redirect(url_for('welcome',name=user))
+    else:
+        user =request.args.get('name')
+        return redirect(url_for('welcome',name=user))
+
+if __name__=='__main__':
+    app.run(debug=True)
